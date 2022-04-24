3b6a80e73457e3537ca4454e9781b7cf5a32efb4
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-04-23 22:59:08 -0500

    Publicando resultados del final de la clase 6

--- asincronismo-js/src/callback/challenge.js
+++ asincronismo-js/src/callback/challenge.js
@@ -1,9 +1,9 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-let API = 'https://rickandmortyapi.com/api/character';
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback){
     let xhttp = new XMLHttpRequest();
-    xhttp.open('GET, url_api, true');
+    xhttp.open('GET', url_api, true);
     xhttp.onreadystatechange = function(event){
         if(xhttp.readyState === 4){
             if(xhttp.status === 200){
@@ -17,3 +17,22 @@ function fetchData(url_api, callback){
     };
     xhttp.send();
 }
+
+fetchData(API,function (error1, data1) {
+    if(error1) return console.error(error1);
+
+    fetchData(API + data1.results[0].id,function (error2, data2) {
+        if(error2) return console.error(error2);
+    
+        fetchData(data2.origin.url,function (error3, data3) {
+            if(error3) return console.error(error3);
+        
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+            
+        });
+        
+    });
+   
+});
