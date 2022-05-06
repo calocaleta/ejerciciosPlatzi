116d0a9631b9fc21b50fc998e8c96ca8071205ee
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-05-06 18:12:49 -0500

    Publicando resultados del final de la clase 5

--- react-practico/react-shop/src/components/App.jsx
+++ react-practico/react-shop/src/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const App = () => {
-    return (
-        <h1>Hola mundo!</h1>
-    );
+	return (
+		<h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>
+	);
 }
 
 export default App;
