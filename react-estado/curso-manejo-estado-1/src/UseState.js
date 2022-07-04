ae6c9bcc842d08028c4cd8f57f7ee31ceae3b8db
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-07-03 23:43:06 -0500

    Publicando resultados del final de la clase 2

---
+++ react-estado/curso-manejo-estado-1/src/UseState.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UseState = () => {
+    return (
+    <div>
+        <h2>Eliminar UseState</h2>
+        <p>Por favor, escribe el código de seguridad.</p>
+
+        <input placeholder='Código de seguridad' />
+        <button>Comprobar</button>
+    </div>
+    );
+};
+
+export {UseState};
